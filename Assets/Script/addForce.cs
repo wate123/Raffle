55181cdad5d3e8bebb6b13348aef163b52eb315d
wate123 <junlin0604@gmail.com>
2018-02-19 14:07:04 -0600

    fix

--- Assets/Script/addForce.cs
+++ Assets/Script/addForce.cs
@@ -12,6 +12,6 @@ public class addForce : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddForce(transform.forward * thrust, ForceMode.Acceleration);
+        rb.AddForce(transform.forward* thrust, ForceMode.Acceleration);
     }
 }
--- Assets/Script/spawnObject.cs
+++ Assets/Script/spawnObject.cs
@@ -1,106 +1,54 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class spawnObject : MonoBehaviour {
-    public GameObject gold;
+    public Rigidbody rb;
+    public GameObject block1;
+    Vector3 gravity;
 
-    enum ModeSwitching { Start, Acceleration };
-    ModeSwitching m_ModeSwitching;
+    public int worldWidth = 40;
+    public int people;
 
-    Vector3 m_StartPos, m_StartForce;
-    Vector3 m_NewForce;
-    Rigidbody m_Rigidbody;
+    public float spawnSpeed = 0;
 
-    public float gridX = 5f;
-    public float gridY = 5f;
-    public float spacing = 2f;
-    string m_ForceXString, m_ForceYString;
-    float m_ForceX, m_ForceY;
-    float m_Result;
+    
 
-    private void spawn()
+    void Start()
     {
-        for (int y = 0; y < gridY; y++)
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        StartCoroutine(CreateWorld());
+        
+    }
+
+    IEnumerator CreateWorld()
+    {
+        for (int x = 0; x < worldWidth; x++)
         {
-            for (int x = 0; x < gridX; x++)
+            yield return new WaitForSeconds(spawnSpeed);
+            for (int z = 0; z < Math.Ceiling(Math.Sqrt(people)); z++)
             {
-                Vector3 pos = new Vector3(x, 0, y) * spacing;
-                Instantiate(gold, pos, Quaternion.identity);
+                GameObject block = Instantiate(block1, Vector3.zero, block1.transform.rotation) as GameObject;
+                block.transform.parent = transform;
+                block.transform.localPosition = new Vector3(x*1.1F, 0, z*1.1F);
             }
         }
     }
 
-    private void Start()
+    void Update()
     {
-        spawn();
-    }
-    private void Update()
-    {
-        //If the current mode is not the starting mode (or the GameObject is not reset), the force can change
-        if (m_ModeSwitching != ModeSwitching.Start)
+        Physics.gravity = gravity;
+
+        if (Input.GetMouseButtonDown(0))
         {
-            //The force changes depending what you input into the text fields
-            m_NewForce = new Vector3(m_ForceX, m_ForceY, 0);
+            rb.isKinematic = false;
+            rb.detectCollisions = true;
+            gravity.x = 0;
+            gravity.y = -20;
+            gravity.z = 0;
         }
-
-        //Here, switching modes depend on button presses in the Game mode
-        switch (m_ModeSwitching)
-        {
-            //This is the starting mode which resets the GameObject
-            case ModeSwitching.Start:
-                //This resets the GameObject and Rigidbody to their starting positions
-                transform.position = m_StartPos;
-                m_Rigidbody.transform.position = m_StartForce;
-                //This resets the velocity of the Rigidbody
-                m_Rigidbody.velocity = new Vector3(0f, 0f, 0f);
-                break;
-
-            //These are the modes ForceMode can force on a Rigidbody
-            //This is Acceleration mode
-            case ModeSwitching.Acceleration:
-                //The function converts the text fields into floats and updates the Rigidbody’s force
-                MakeCustomForce();
-                //Use Acceleration as the force on the Rigidbody
-                m_Rigidbody.AddForce(m_NewForce, ForceMode.Acceleration);
-                break;
-        }
-    }
-
-    void OnGUI()
-    {
-        //Getting the inputs from each text field and storing them as strings
-        m_ForceXString = GUI.TextField(new Rect(300, 10, 200, 20), m_ForceXString, 25);
-        m_ForceYString = GUI.TextField(new Rect(300, 30, 200, 20), m_ForceYString, 25);
-
-        //Press the button to reset the GameObject and Rigidbody
-        if (GUI.Button(new Rect(100, 0, 150, 30), "Reset"))
-        {
-            //This switches to the start/reset case
-            m_ModeSwitching = ModeSwitching.Start;
-        }
-
-        //When you press the Acceleration button, switch to Acceleration mode
-        if (GUI.Button(new Rect(100, 30, 150, 30), "Apply Acceleration"))
-        {
-            //Switch to Acceleration (apply acceleration force to GameObject)
-            m_ModeSwitching = ModeSwitching.Acceleration;
-        }
-    }
-
-    //Changing strings to floats for the forces
-    float ConvertToFloat(string Name)
-    {
-        float.TryParse(Name, out m_Result);
-        return m_Result;
-    }
-
-    //Set the converted float from the text fields as the forces to apply to the Rigidbody
-    void MakeCustomForce()
-    {
-        //This converts the strings to floats
-        m_ForceX = ConvertToFloat(m_ForceXString);
-        m_ForceY = ConvertToFloat(m_ForceYString);
     }
 
 }
